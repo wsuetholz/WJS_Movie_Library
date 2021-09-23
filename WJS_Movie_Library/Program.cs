46c94fcb11f6d3c8eb6630f595b2cc1c4b8b4ed6
William Suetholz <bills@mwes.com>
2021-09-23 13:20:01 -0500

    Rearranged code to actually compile..  Used Visual Studio to accomplish..

---
+++ WJS_Movie_Library/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CsvHelper;
+using WJS_Movie_Library.Model;
+using WJS_Movie_Library.Services;
+
+namespace WJS_Movie_Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fName = "Data/movies.csv";
+
+            MovieService movieService = new MovieService(fName);
+            foreach (Movie movie in movieService.GetMovies())
+            {
+                Console.WriteLine(movie);
+            }
+        }
+    }
+}
