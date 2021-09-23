febb7511912c70acaefe8017aa2652a1b4d1fa5e
William S <wj@suetholz.net>
2021-09-23 12:54:41 -0500

    Version with compiling issues.

---
+++ Services/WJS_Movie_Library.Services/MovieService.cs
@@ -0,0 +1,39 @@
+using System.Globalization;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using CsvHelper;
+using WJS_Movie_Library.Model;
+
+namespace WJS_Movie_Library.Services
+{
+    public class MovieService 
+    {
+        private IList<Movie> movieList;
+
+        public MovieService(string fname) 
+        {
+            using (var reader = new StreamReader(fname))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<Movie>();
+                    movieList = new List<Movie>(records);
+                }
+            
+            }
+        }
+
+        public IList<Movie> GetMovies()
+        {
+            return movieList;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+
+        }
+    }
+}
