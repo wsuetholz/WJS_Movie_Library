8410ab48828701560e1fdd186535b7a66b6e4cc7
William S <wj@suetholz.net>
2021-09-23 21:34:46 -0500

    Added UnitTest

---
+++ WJS_Movie_Library.Tests/MovieService_UnitTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace WJS_Movie_Library.Tests
+{
+    public class MovieService_UnitTests
+    {
+        [Fact]
+        public void TitleExists()
+        {
+        }
+
+        [Fact]
+        public void TitleDoesntExist()
+        {
+        }
+    }
+}
